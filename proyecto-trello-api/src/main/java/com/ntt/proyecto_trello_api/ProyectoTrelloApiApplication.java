f018288b4cd2869d5d482b301b6fcf56941c8820
juanjice29 <jm.jimenezc12@uniandes.edu.co>
2025-06-19 09:22:25 -0500

    Spring initializer config

---
+++ proyecto-trello-api/src/main/java/com/ntt/proyecto_trello_api/ProyectoTrelloApiApplication.java
@@ -0,0 +1,13 @@
+package com.ntt.proyecto_trello_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTrelloApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTrelloApiApplication.class, args);
+	}
+
+}
